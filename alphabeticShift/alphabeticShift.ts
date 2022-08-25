a1e9bd7e89f18959297a4617f971af428ad49b9a
mcmicko <mcmzrc@gmail.com>
2022-08-25 17:47:10 +0200

    Second Day

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,66 @@
 export function alphabeticShift(inputString: string): string {
+  const alphabet: string[] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
+  let inputShifted = inputString.split("");
+
+  for (let i = 0; i < inputShifted.length; i++) {
+    let index = 0;
+
+    if (inputShifted[i] !== "z") {
+      index = alphabet.indexOf(inputShifted[i]) + 1;
+    }
+    // console.log(index)
+    // console.log(inputShifted[i])
+    inputShifted[i] = alphabet[index];
+  }
+
+  return inputShifted.join("");
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift("crazyz"));
+
+export function alphabeticShift2(inputString: string): string {
+  const alphabet: object = {
+    a: "b",
+    b: "c",
+    c: "d",
+  };
+
+  let inputShifted = inputString.split("");
+
+  for (let i = 0; i < inputShifted.length; i++) {
+    console.log(inputShifted[i]);
+    inputShifted[i] = alphabet[inputShifted[i]];
+  }
+
+  return inputShifted.join("");
+}
+
+console.log(alphabeticShift2("crazyz"));
--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,15 @@
 export function alternatingSums(a: number[]): number[] {
-
+  let evenSum = 0;
+  let oddSum = 0;
+  a.forEach((e, i) => {
+    if (i % 2 === 0) {
+      evenSum += e;
+    } else {
+      oddSum += e;
+    }
+  });
+  return [evenSum, oddSum];
+  // return a.filter((e,i) => e.indexOf(i%2))
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
