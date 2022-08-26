a87d719a7b0c7d0f1e2d716873e72f2752bb2dcc
mcmicko <mcmzrc@gmail.com>
2022-08-26 22:11:46 +0200

    Third Day

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,19 @@
-export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+export function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+  const friendWeakest =
+    friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  const frieandStrongest =
+    friendsLeft >= friendsLeft ? friendsLeft : friendsRight;
 
+  return yourStrongest === frieandStrongest && yourWeakest === friendWeakest;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,19 @@
 export function arrayChange(inputArray: number[]): number {
+  let count = 0;
 
+  for (let i = 0; i < inputArray.length; i++) {
+    console.log(inputArray);
+    if (inputArray[i] >= inputArray[i + 1]) {
+      console.log(inputArray);
+      const difference = inputArray[i] + 1 - inputArray[i + 1];
+      inputArray[i + 1] = inputArray[i] + 1;
+      console.log(difference);
+      count += difference;
+    }
+  }
+  return count;
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
+// console.log(arrayChange([1, 2, 3]));
+// console.log(arrayChange([1, 2, 1, 3, 5]));
