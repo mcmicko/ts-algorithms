26619da1882a02041f735bdd33a1aff92bbdf636
mcmicko <mcmzrc@gmail.com>
2022-08-28 21:56:39 +0200

    fifth day

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,27 @@
 export function arrayPreviousLess(items: number[]): number[] {
+  let lessThanList: number[] = [];
 
+  for (let i = items.length - 1; i >= 0; i--) {
+    for (let j = i; j >= 0; j--) {
+      if (items[i] > items[j]) {
+        lessThanList.unshift(items[j]);
+        break;
+      } else if (j === 0) {
+        lessThanList.unshift(-1);
+      }
+    }
+  }
+
+  return lessThanList;
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+function arrayPreviousLess2(items: number[]): number[] {
+  const result = items.map((item: number, i: number) =>
+    item > items[i - 1] ? items[i - 1] : -1
+  );
+
+  return result;
+}
+
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess2([3, 5, 2, 4, 5]));
--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,20 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  inputArray.forEach((e, i) => {
+    if (e === elemToReplace) {
+      inputArray[i] = substitutionElem;
+    }
+  });
 
+  return inputArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+function arrayReplace2(a: number[], b: number, c: number): any {
+  return a.map((num: number) => (num === b ? c : num));
+}
+
+console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace2([1, 2, 1], 1, 3));
